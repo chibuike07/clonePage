74a79f497e7540f5b739821df4fa9501bfaa4f64
hobbyluv <princewillchime43@gmail.com>
2020-07-08 15:50:33 +0100

    Updates

---
+++ precache-manifest.562fc741b8c6179bc5c05123fd1c0051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e60beba6d8400d800a74a3ec1ca9cba",
+    "url": "/clonePage/index.html"
+  },
+  {
+    "revision": "11240168084d3324b0bd",
+    "url": "/clonePage/static/css/main.efb556d9.chunk.css"
+  },
+  {
+    "revision": "ab7fe04f1dbb7fd358de",
+    "url": "/clonePage/static/js/2.579f0a26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clonePage/static/js/2.579f0a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11240168084d3324b0bd",
+    "url": "/clonePage/static/js/main.da76de61.chunk.js"
+  },
+  {
+    "revision": "1a336b47f8bff4a800cb",
+    "url": "/clonePage/static/js/runtime-main.825c2484.js"
+  },
+  {
+    "revision": "61546251a70511b03cf9bb5ea1feab18",
+    "url": "/clonePage/static/media/studL1.61546251.png"
+  }
+]);
